aac351e51b7369a924a0799c7ef04c6d15f5ca9b
albros82 <122279846+albros82@users.noreply.github.com>
2023-03-17 14:31:32 +0100

    initial commit

---
+++ src/main/java/albert/finalproyectdam/SecondaryController.java
@@ -0,0 +1,12 @@
+package albert.finalproyectdam;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module albert.finalproyectdam {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens albert.finalproyectdam to javafx.fxml;
+    exports albert.finalproyectdam;
+}
